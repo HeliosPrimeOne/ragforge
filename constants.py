008449164daa938363726d17260e7c0f3e6489dd
Helios <89754687+HeliosPrimeOne@users.noreply.github.com>
2023-12-27 19:05:22 -0800

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,66 @@
+import os
+from chromadb.config import Settings
+from langchain.document_loaders import CSVLoader, PDFMinerLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader
+from langchain.document_loaders import UnstructuredFileLoader, UnstructuredMarkdownLoader
+
+
+
+ROOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
+# Define the folder for storing database
+SOURCE_DIRECTORY = f"{ROOT_DIRECTORY}/SOURCE_DOCUMENTS"
+
+PERSIST_DIRECTORY = f"{ROOT_DIRECTORY}/DB"
+
+
+# Can be changed to a specific number
+INGEST_THREADS = os.cpu_count() or 8
+
+# Define the Chroma settings
+CHROMA_SETTINGS = Settings(
+    anonymized_telemetry=False,
+    is_persistent=True,
+)
+
+DOCUMENT_MAP = {
+    ".txt": TextLoader,
+    ".md": UnstructuredMarkdownLoader,
+    ".py": TextLoader,
+    ".pdf": UnstructuredFileLoader,
+    ".csv": CSVLoader,
+    ".xls": UnstructuredExcelLoader,
+    ".xlsx": UnstructuredExcelLoader,
+    ".docx": Docx2txtLoader,
+    ".doc": Docx2txtLoader,
+}
+
+# Default Instructor Model
+EMBEDDING_MODEL_NAME = "hkunlp/instructor-large"  # Uses 1.5 GB of VRAM (High Accuracy with lower VRAM usage)
+
+####
+#### OTHER EMBEDDING MODEL OPTIONS
+####
+
+# EMBEDDING_MODEL_NAME = "hkunlp/instructor-xl" # Uses 5 GB of VRAM (Most Accurate of all models)
+# EMBEDDING_MODEL_NAME = "intfloat/e5-small-v2" # Uses 5 GB of VRAM (Most Accurate of all models)
+# EMBEDDING_MODEL_NAME = "intfloat/e5-large-v2" # Uses 1.5 GB of VRAM (A little less accurate than instructor-large)
+# EMBEDDING_MODEL_NAME = "intfloat/e5-base-v2" # Uses 0.5 GB of VRAM (A good model for lower VRAM GPUs)
+# EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2" # Uses 0.2 GB of VRAM (Less accurate but fastest - only requires 150mb of vram)
+
+####
+#### MULTILINGUAL EMBEDDING MODELS
+####
+
+# EMBEDDING_MODEL_NAME = "intfloat/multilingual-e5-large" # Uses 2.5 GB of VRAM
+# EMBEDDING_MODEL_NAME = "intfloat/multilingual-e5-base" # Uses 1.2 GB of VRAM
+
+
+#### GPU VRAM Memory required for LLM Models (ONLY) by Billion Parameter value (B Model)
+#### Does not include VRAM used by Embedding Models - which use an additional 2GB-7GB of VRAM depending on the model.
+####
+#### (B Model)   (float32)    (float16)    (GPTQ 8bit)         (GPTQ 4bit)
+####    7b         28 GB        14 GB       7 GB - 9 GB        3.5 GB - 5 GB
+####    13b        52 GB        26 GB       13 GB - 15 GB      6.5 GB - 8 GB
+####    32b        130 GB       65 GB       32.5 GB - 35 GB    16.25 GB - 19 GB
+####    65b        260.8 GB     130.4 GB    65.2 GB - 67 GB    32.6 GB -  - 35 GB
+
---
+++ run.py
@@ -0,0 +1,132 @@
+import os
+import time
+import logging
+import click
+import openai
+import torch
+import utils
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.llms import OpenAI
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from langchain.vectorstores import Chroma
+from constants import (
+    EMBEDDING_MODEL_NAME,
+    PERSIST_DIRECTORY,
+    CHROMA_SETTINGS
+)
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+
+
+def load_remote_model(api_key, base_url):
+    openai.api_key = api_key
+    openai.base_url = base_url
+
+def retrieval_qa_pipeline(device_type, use_history):
+    embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": device_type})
+    db = Chroma(
+        persist_directory=PERSIST_DIRECTORY,
+        embedding_function=embeddings,
+        client_settings=CHROMA_SETTINGS
+    )
+    retriever = db.as_retriever()
+    
+
+
+
+
+    llm_config = OpenAI(
+        api_key="YOUR_API_KEY_HERE",   # API key exposed for this example.  Use env file for real key.  Local LLM's take any input as key.
+        base_url="http://localhost:1234/v1",
+        # seed=1,
+        timeout=600,
+        temperature=0.3, # Adjust from 0.1 - 0.9  --Lower = more to the point, higher = more creative.
+        max_tokens=-1,
+    )
+
+    llm = llm_config
+
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True,
+        callbacks=callback_manager,
+        
+    )
+
+    return qa
+
+@click.command()
+@click.option(
+    "--device_type",
+    default="cuda" if torch.cuda.is_available() else "cpu",
+    type=click.Choice([
+        "cpu", "cuda", "ipu", "xpu", "mkldnn", "opengl", "opencl", "ideep", "hip", "ve", "fpga", "ort", "xla",
+        "lazy", "vulkan", "mps", "meta", "hpu", "mtia",
+    ]),
+    help="Device to run on. (Default is cuda)",
+)
+@click.option(
+    "--show_sources",
+    "-s",
+    # default=True,
+    is_flag=True,
+    help="Show sources along with answers (Default is False)",
+)
+@click.option(
+    "--use_history",
+    "-h",
+    default=False,
+    is_flag=True,
+    help="Use history (Default is False)",
+)
+@click.option(
+    "--save_qa",
+    is_flag=True,
+    help="Whether to save Q&A pairs to a CSV file (Default is False)",
+)
+
+def main(device_type, show_sources, use_history, save_qa):
+    logging.info(f"Running on: {device_type}")
+    logging.info(f"Display Source Documents set to: {show_sources}")
+    logging.info(f"Use history set to: {use_history}")
+
+    qa = retrieval_qa_pipeline(device_type, use_history)
+    
+    while True:
+        query = input("\nEnter a query: ")
+        if query == "exit":
+            break
+        res = qa(query)
+        answer, docs = res["result"], res["source_documents"]
+
+        print("\n\n> Question:")
+        print(query)
+        print("\n> Answer:")
+        print(answer)
+
+
+
+        if show_sources:
+            time.sleep(2)
+
+            print("\033[38;2;255;165;0mLoading Sources from response...\033[0m")
+
+            print("----------------------------------SOURCE DOCUMENTS---------------------------")
+            for document in docs:
+                print("\n> " + document.metadata["source"] + ":")
+                print(document.page_content)
+            print("----------------------------------SOURCE DOCUMENTS---------------------------")
+
+        if save_qa:
+            utils.log_to_csv(query, answer)
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
+    )
+    main()
